0392de5fe58c819975d924ff776c8fa3d540944b
altair823 <dlsrks0734@gmail.com>
2024-01-21 15:54:41 +0900

    fix script

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
-java -jar -Dspring.config.location=/home/altair823/app/application-oauth.properties,\
+nohup java -jar -Dspring.config.location=/home/altair823/app/application-oauth.properties,\
 /home/altair823/app/application-real-db.properties \
 -Dspring.profiles.active=real build/libs/blog-0.0.1-SNAPSHOT.jar
